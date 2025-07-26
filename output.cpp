4ca133773b0e5aa0649ed001f3a2ce898a36e0cd
Saad Arshad <robotichuman830@gmail.com>
2025-07-26 01:44:16 -0700

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\n Oxygen Programmming Language\n" << endl;
+    cout << "Version 0.1\n" << endl;
+    cout << "Created by Saad Arshad\n" << endl;
+    return 0;
+}
---
+++ test.ox
@@ -0,0 +1,7 @@
+
+Start() {
+     
+    Print "/n Oxygen Programmming Language"
+    Print "Version 0.1" 
+    Print "Created by Saad Arshad"
+}
