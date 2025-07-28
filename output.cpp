887bd56e200447f00ab91fe3be1ad1fe013010a9
Saad Arshad <robotichuman830@gmail.com>
2025-07-28 03:52:14 -0700

    Add files via upload

--- output.cpp
+++ output.cpp
@@ -1,9 +1,49 @@
+#include <fstream>
 #include <iostream>
 using namespace std;
 
+void oxy_cpp_block_1() {
+
+//Oxygen is invalid here , C++ only
+cout << "Hello , C++2" ;
+
+
+}
+
+void oxy_cpp_block_2() {
+
+//Oxygen is invalid here , C++ only
+cout << "Hello , Cpp block 2 " ;
+
+
+}
+
+void oxy_cpp_block_3() {
+
+//Oxygen is invalid here , C++ only
+cout << "Hello , C++" ;
+
+
+}
+
 int main() {
-    cout << "\n Oxygen Programmming Language\n" << endl;
-    cout << "Version 0.1\n" << endl;
-    cout << "Created by Saad Arshad\n" << endl;
+    oxy_cpp_block_1();
+    oxy_cpp_block_2();
+    oxy_cpp_block_3();
+    cout << "\n Oxygen Programming Language \n \n A Modernized Version of C++ \n" << endl;
+    cout << " Currently in Development \n" << endl;
+    cout << "Version 0.2 \n" << endl;
+    cout << "Created by Saad Arshad \n" << endl;
+    switch (A) {
+        case A:
+    cout << "Excellent!" << endl;
+            break;
+        case B:
+    cout << "Good!" << endl;
+            break;
+        default:
+    cout << "Needs improvement." << endl;
+            break;
+    }
     return 0;
 }
