00506eb610888a4d6956bdad8ac028c5e4739fb4
Saad Arshad <robotichuman830@gmail.com>
2025-08-02 07:40:40 -0700

    Add files via upload

--- output.cpp
+++ output.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void oxy_cpp_block_1() {
 
 //Oxygen is invalid here , C++ only
-cout << "Hello , C++2" ;
+cout << " Hello , C++ " ;
 
 
 }
@@ -13,7 +13,7 @@ cout << "Hello , C++2" ;
 void oxy_cpp_block_2() {
 
 //Oxygen is invalid here , C++ only
-cout << "Hello , Cpp block 2 " ;
+cout << " Hello , Cpp block 2 " ;
 
 
 }
@@ -34,15 +34,17 @@ int main() {
     cout << " Currently in Development \n" << endl;
     cout << "Version 0.2 \n" << endl;
     cout << "Created by Saad Arshad \n" << endl;
-    switch (A) {
-        case A:
-    cout << "Excellent!" << endl;
+    cout << (46 + 5 - 1) << endl;
+    cout << (60 * 2 + 2) << endl;
+    switch (24 + 2) {
+        case 24:
+    cout << "\n Excellent! \n" << endl;
             break;
-        case B:
-    cout << "Good!" << endl;
+        case 26:
+    cout << "\n Good! \n" << endl;
             break;
         default:
-    cout << "Needs improvement." << endl;
+    cout << "\n   Needs improvement. \n" << endl;
             break;
     }
     return 0;
